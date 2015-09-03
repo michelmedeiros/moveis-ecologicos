bbe15ecf10ed9e20d7e5570c26ecf048c604a586
michelmedeiros <michel.tds@gmail.com>
2015-09-02 22:02:15 -0300

    Alterando JS

--- js/principal.js
+++ js/principal.js
@@ -1,8 +1,10 @@
 var banners = ["Do lixo ao luxo!", "Reaproveitar é aproveitar!"];
 var bannerAtual = 0;
+
 function trocaBanner() {
 	bannerAtual = (bannerAtual + 1) % 2;
-	document.querySelector('h2#mensagem').textContent =
-	banners[bannerAtual];
+	document.querySelector('h2#mensagem').textContent = banners[bannerAtual];
+	
 }
+
 setInterval(trocaBanner, 1000);
